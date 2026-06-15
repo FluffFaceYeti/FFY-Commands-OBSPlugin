6b22db3faf8e015fdc9dcf52f73239a515ca9353
FlufffaceYeti <109767599+FluffFaceYeti@users.noreply.github.com>
2026-06-15 23:07:29 +0100

    Add files via upload

---
+++ data/list-commands/customlistcommands.js
@@ -0,0 +1,19 @@
+const customitem = {
+  customitem: {
+    list: [
+   "items1",
+   "item2",
+    ],
+    label: "today your customitem is",
+  },
+};
+const customitem2 = {
+  customitem2: {
+    list: [
+    "items1",
+    "item2",
+    ],
+    label: "today your customitem2 is",
+  },
+};
+export { customitem, customitem2};
